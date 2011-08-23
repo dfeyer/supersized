4ccd975858c49278c0c77701ef16fa525f97de1f
Dominique Feyer <dominique.feyer@reelpeek.net>
2011-08-23 18:15:47 +0200

    [TASK] Move the doc to the doc directory to be compatible with the TER

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "supersized".
 #
-# Auto generated 21-08-2011 17:37
+# Auto generated 22-08-2011 12:37
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'This extension include the jQuery Supersized plugin to add fullscreen background to your site.',
 	'category' => 'fe',
 	'shy' => 0,
-	'version' => '1.0.1',
+	'version' => '1.0.2',
 	'dependencies' => 'extbase,fluid',
 	'conflicts' => '',
 	'priority' => '',
@@ -44,7 +44,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:25:{s:10:"README.rst";s:4:"957c";s:12:"ext_icon.gif";s:4:"02f9";s:17:"ext_localconf.php";s:4:"12a9";s:14:"ext_tables.php";s:4:"eb39";s:14:"ext_tables.sql";s:4:"6817";s:43:"Classes/Controller/BackgroundController.php";s:4:"d0a4";s:29:"Classes/Domain/Model/Page.php";s:4:"6402";s:33:"Classes/Domain/Model/Resource.php";s:4:"5e12";s:44:"Classes/Domain/Repository/PageRepository.php";s:4:"0873";s:48:"Classes/Domain/Repository/ResourceRepository.php";s:4:"095f";s:28:"Classes/Utility/UserFunc.php";s:4:"dca9";s:39:"Classes/ViewHelpers/ImageViewHelper.php";s:4:"f8c2";s:30:"Configuration/TCA/Resource.php";s:4:"22c9";s:38:"Configuration/TypoScript/constants.txt";s:4:"6f3f";s:34:"Configuration/TypoScript/setup.txt";s:4:"f712";s:31:"Documentation/Manual/manual.pdf";s:4:"ed4d";s:31:"Documentation/Manual/manual.sxw";s:4:"a178";s:43:"Resources/Private/Language/locallang_db.xml";s:4:"d630";s:38:"Resources/Private/Layouts/Default.html";s:4:"3562";s:37:"Resources/Private/Partials/Index.html";s:4:"d41d";s:53:"Resources/Private/Templates/Background/Configure.html";s:4:"e011";s:62:"Resources/Public/Icons/tx_supersized_domain_model_resource.gif";s:4:"43db";s:41:"Resources/Public/JavaScript/jquery.min.js";s:4:"3c37";s:56:"Resources/Public/JavaScript/supersized.core.3.2.0.min.js";s:4:"485c";s:47:"Resources/Public/StyleSheet/supersized.core.css";s:4:"5bdd";}',
+	'_md5_values_when_last_written' => 'a:25:{s:10:"README.rst";s:4:"957c";s:12:"ext_icon.gif";s:4:"02f9";s:17:"ext_localconf.php";s:4:"12a9";s:14:"ext_tables.php";s:4:"5f91";s:14:"ext_tables.sql";s:4:"6817";s:43:"Classes/Controller/BackgroundController.php";s:4:"d0a4";s:29:"Classes/Domain/Model/Page.php";s:4:"6402";s:33:"Classes/Domain/Model/Resource.php";s:4:"5e12";s:44:"Classes/Domain/Repository/PageRepository.php";s:4:"0873";s:48:"Classes/Domain/Repository/ResourceRepository.php";s:4:"095f";s:35:"Classes/Utility/UserFuncUtility.php";s:4:"3231";s:39:"Classes/ViewHelpers/ImageViewHelper.php";s:4:"f8c2";s:30:"Configuration/TCA/Resource.php";s:4:"22c9";s:38:"Configuration/TypoScript/constants.txt";s:4:"6f3f";s:34:"Configuration/TypoScript/setup.txt";s:4:"f712";s:31:"Documentation/Manual/manual.pdf";s:4:"ed4d";s:31:"Documentation/Manual/manual.sxw";s:4:"a178";s:43:"Resources/Private/Language/locallang_db.xml";s:4:"d630";s:38:"Resources/Private/Layouts/Default.html";s:4:"3562";s:37:"Resources/Private/Partials/Index.html";s:4:"d41d";s:53:"Resources/Private/Templates/Background/Configure.html";s:4:"e011";s:62:"Resources/Public/Icons/tx_supersized_domain_model_resource.gif";s:4:"43db";s:41:"Resources/Public/JavaScript/jquery.min.js";s:4:"3c37";s:56:"Resources/Public/JavaScript/supersized.core.3.2.0.min.js";s:4:"485c";s:47:"Resources/Public/StyleSheet/supersized.core.css";s:4:"5bdd";}',
 	'suggests' => array(
 	),
 );
